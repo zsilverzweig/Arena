eb2ae1b4a562d9a88dc1ef7a62b323d52c8a4364
zsilverzweig <silverzweig@gmail.com>
2023-12-02 17:24:06 -0500

    Fixed bug with final Score

--- Assets/Scripts/Game Management/EndScreen.cs
+++ Assets/Scripts/Game Management/EndScreen.cs
@@ -5,17 +5,13 @@ namespace Game_Management
 {
     public class EndScreen : MonoBehaviour
     {
-        private FinalScore _finalScore;
+        [SerializeField] 
+        private TextMeshProUGUI finalScore;
     
-        // Start is called before the first frame update
-        void Start()
-        {
-            _finalScore = GetComponentInChildren<FinalScore>();
-        }
 
         public void SetScore(int experience)
         {
-            _finalScore.GetComponentInChildren<TextMeshProUGUI>().text = experience.ToString("D6");
+            finalScore.text = experience.ToString("D6");
         }
     }
 }
---
+++ Assets/readme.md
@@ -0,0 +1,10 @@
+ARENA
+
+To Do
+- [ ] Update UI to work on any mobile device
+- [ ] Draw mountain sprites
+- [ ] Enable camera to shift to follow player when at the edge of the map
+- [ ] Add Multiplayer Support!
+- [ ] Add PvP Zones
+- [ ] Add Fog of War
+- [ ] Add inventory with slots for Player
